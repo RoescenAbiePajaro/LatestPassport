7745b7860ce89b51cbfa3ff6b91930d8597ca270
Longos26 <romarlongos026@gmail.com>
2025-03-10 12:31:01 +0800

    qqq

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import SignIn from './pages/SignIn';
-import Dashboard from './pages/Dashboard';
-import Projects from './pages/Projects';
-import SignUp from './pages/SignUp';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import PrivateRoute from './components/PrivateRoute';
-import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
-import CreatePost from './pages/CreatePost';
-import UpdatePost from './pages/UpdatePost';
-import PostPage from './pages/PostPage';
-import ScrollToTop from './components/ScrollToTop';
-import Search from './pages/Search';
+import Navbar from "./components/Header";
 
-export default function App() {
+const App = () => {
   return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/search' element={<Search />} />
-        <Route element={<PrivateRoute />}>
-          <Route path='/dashboard' element={<Dashboard />} />
-        </Route>
-        <Route element={<OnlyAdminPrivateRoute />}>
-          <Route path='/create-post' element={<CreatePost />} />
-          <Route path='/update-post/:postId' element={<UpdatePost />} />
-        </Route>
-
-        <Route path='/projects' element={<Projects />} />
-        <Route path='/post/:postSlug' element={<PostPage />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <div className="px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64">
+      {/* NAVBAR */}
+      <Navbar />
+      {/* BREADCRUMB */}
+      {/* INTRODUCTION */}
+      {/* FEATURED POSTS */}
+      {/* POST LIST */}
+    </div>
   );
-}
+};
+
+export default App;
